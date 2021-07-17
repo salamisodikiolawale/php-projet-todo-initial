65fa63bd572018201bbd9577d0aa628fb5d99dc5
Salami <salamisodikiolawale@gmail.com>
2021-07-17 11:27:57 +0200

    Last commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php require_once 'includes/head.php' ?>
+    <title>Todo</title>
+</head>
+
+<body>
+    <div class="container">
+        <?php require_once 'includes/header.php' ?>
+        <div class="content">
+            <div class="todo-container">
+                <h1>Ma Todo</h1>
+                <div class="todo-form"></div>
+                <div class="todo-list"></div>
+            </div>
+        </div>
+        <?php require_once 'includes/footer.php' ?>
+    </div>
+
+</body>
+
+</html>
